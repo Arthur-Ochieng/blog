456fe6a48aff7e8fd125dc101b86a25a30ee2271
Arthur-Ochineg <johnarthur016@gmail.com>
2022-06-29 11:11:29 +0300

    added new routes for the user-blog

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Routes, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+
+
+function App() {
+  // const title = "Welcome to the new blog";
+  // const likes = 50;
+
+  return (
+    <>
+    <h1>Hallo</h1>
+    </>
+  );
+}
+
+export default App;
